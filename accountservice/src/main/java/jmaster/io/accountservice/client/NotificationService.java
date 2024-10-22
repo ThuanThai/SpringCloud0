c6f7afa7f3a35460631c0bfb2245315265337786
ThaiThuan <thaithuan14501@gmail.com>
2024-10-22 08:44:34 +0700

    upload gateway-service

--- accountservice/src/main/java/jmaster/io/accountservice/client/NotificationService.java
+++ accountservice/src/main/java/jmaster/io/accountservice/client/NotificationService.java
@@ -6,8 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 
 @FeignClient(name = "notification-service", url = "http://localhost:9083", fallback = NotificationServiceImpl.class)
 public interface NotificationService {
